53ea461b5f6f75bf68c6c14d37058b670e58cbcf
Oscar Bello Valenzo <31903300+oscarvalenzo@users.noreply.github.com>
2019-02-20 14:56:39 -0500

    Add files via upload

---
+++ ARRAY/js/main.js
@@ -0,0 +1,22 @@
+let event =[
+// event, index 0
+  {
+  date:"1/14/2019",
+  name: "IDEATION",
+  description:"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+  },
+  assets:
+  involved departaments:[
+    {name:ART, progress:80},
+    {name:CONSTRUCTION, progress:20},
+    {name:PAINT, progress:15},
+    {name:PROPS, progress:0},
+    {name:SET DECORATORS, progress:0},
+    {name:STUNTS, progress:0},
+    {name:COSTUMES, progress:0},
+    {name:LIGHTING, progress:45},
+    {name:GRIPS, progress:0},
+    {name:TRANSPORT, progress:100}
+  ]
+
+  ]
